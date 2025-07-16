529190e223bf7237c4eb412fb0e0a9bea76c765c
M-BEDH <mel.b.ms@pm.me>
2025-07-16 09:24:01 +0200

    horloge analogique et numérique

---
+++ index.js
@@ -0,0 +1,61 @@
+  let alarmeDéjàDéclenchée = false;
+  const sonnerie = new Audio("sound/alarm.wav");
+  sonnerie.loop = true;
+
+function updateHorloge() {
+  const date = new Date();
+  const hr = date.getHours();
+  const mn = date.getMinutes();
+  const sec = date.getSeconds();
+    
+  // Formatage à deux chiffres
+  const hrStr = hr < 10 ? "0" + hr : hr;
+  const mnStr = mn < 10 ? "0" + mn : mn;
+  const secStr = sec < 10 ? "0" + sec : sec;
+  
+  // Mise à jour horloge numérique
+  document.getElementById('hrs').innerText = hrStr;
+  document.getElementById('mns').innerText = mnStr;
+  document.getElementById('secs').innerText = secStr;
+  
+  // Mise à jour horloge analogique (aiguilles)
+  const hrRotation = 30 * hr + mn / 2;
+  const minRotation = 6 * mn;
+  const secRotation = 6 * sec;
+  
+  document.getElementById('hr').style.transform = `rotate(${hrRotation}deg)`;
+  document.getElementById('mn').style.transform = `rotate(${minRotation}deg)`;
+  document.getElementById('sc').style.transform = `rotate(${secRotation}deg)`;
+  
+  // Vérifie l'heure d'alarme
+  const alarmTime = document.querySelector('input').value;
+  const currentTime = `${hrStr}:${mnStr}`;
+  if (currentTime === alarmTime) {
+    
+    // ici in sere la sonnerie (dossier soud - fichier BeepTimer)
+    
+    if (!alarmeDéjàDéclenchée) {
+      sonnerie.play().catch(e => console.warn("Son bloqué :", e));
+      alarmeDéjàDéclenchée = true;
+    }
+    console.log("L'alarme a sonné !");
+  }
+}
+
+// Ajout du listener une seule fois
+const btn = document.querySelector('button');
+if (!btn.dataset.listenerAdded) {
+  btn.addEventListener("click", () => {
+    document.querySelector('input').value = "";
+    
+    sonnerie.pause();
+    sonnerie.currentTime = 0;
+    alarmeDéjàDéclenchée = false;
+    console.log("L'alarme a été désactivée");
+  });
+  btn.dataset.listenerAdded = "true";
+}
+
+// Lancer l'horloge
+updateHorloge();
+setInterval(updateHorloge, 1000);
